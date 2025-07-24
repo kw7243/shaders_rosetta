30a20fd9e6c22a8755802151265fff96c8e91f0d
Ubuntu <marhamil@marhamil-gpu-10.1w32o1pjbequrp2so5ln5dpupd.gx.internal.cloudapp.net>
2025-07-24 03:38:52 +0000

    Log outputs to separate file

--- run_match.sh
+++ run_match.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python match.py --device "cuda:0" --gan_mode "styleganxl" --discr_mode "dino_vits16" --discr_path "/datadrive/shaden/project1/FeatureInversion/pretrained_models/dino_shaders_mixup.pth" --discr_modelname "dinoshadersmixup" --batch_size 16 --epochs 100 --class 0 
+python match.py --device "cuda:2" --gan_mode "styleganxl" --discr_mode "dino_vits16" --discr_path "/datadrive/shaden/project1/FeatureInversion/pretrained_models/dino_shaders_kml.pth" --discr_modelname "dinoshaderskml" --batch_size 16 --epochs 100 --class 0 > log.txt 2>&1
